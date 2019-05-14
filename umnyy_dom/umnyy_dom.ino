b1f75fade6309b46b3beb1353559f88d4110d4b7
mikhan808 <kalyachin@mail.ru>
2019-05-14 13:31:17 +0300

    добавлено включение света в спальне с пульта

--- umnyy_dom/umnyy_dom.ino
+++ umnyy_dom/umnyy_dom.ino
@@ -1,14 +1,34 @@
+#include "IRremote.h"
+
+IRrecv irrecv(2); // указываем вывод, к которому подключен приемник
+
+decode_results results;
+bool svet_spalnya;
+int svet_spalnya_pin = 13;
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
-  
+  Serial.begin(9600); // выставляем скорость COM порта
+  irrecv.enableIRIn(); // запускаем прием
+  svet_spalnya = HIGH;
+  pinMode(svet_spalnya_pin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);   // зажигаем светодиод
-  delay(1000);              // ждем секунду
-  digitalWrite(13, LOW);    // выключаем светодиод
-  delay(1000);              // ждем секунду
-
+  if ( irrecv.decode( &results )) { // если данные пришли
+    Serial.println( results.value, HEX ); // печатаем данные
+    if (results.value == 0x2FDA05F)
+    {
+      if (svet_spalnya == HIGH)
+      {
+        svet_spalnya = LOW;
+      }
+      else
+      {
+        svet_spalnya = HIGH;
+      }
+      digitalWrite(svet_spalnya_pin, svet_spalnya);  // зажигаем светодиод
+      delay(1000);
+    }
+    irrecv.resume(); // принимаем следующую команду
+  }
 }
