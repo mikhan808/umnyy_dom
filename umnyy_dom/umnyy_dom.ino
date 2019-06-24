99c4e1f67687d2cfc749b7b40d2aa81ed75c2fa0
Mikhail Kalyashin <kalyachin@mail.ru>
2019-06-25 00:16:52 +0300

    свет по wi-fi на 8 pin Чтоб включить или отключить надо перейти по адресу 192.168.0.17

--- umnyy_dom/umnyy_dom.ino
+++ umnyy_dom/umnyy_dom.ino
@@ -1,21 +1,43 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-const char* ssid = "*****";
+const char* ssid = "****";
 const char* password = "*****";
-IPAddress ip(192,168,1,17);  //статический IP
-IPAddress gateway(192,168,1,1);
+IPAddress ip(192,168,0,17);  //статический IP
+IPAddress gateway(192,168,0,1);
 IPAddress subnet(255,255,255,0);
 ESP8266WebServer server(80);
-const int led = 13;
+bool val = LOW;
+const int led = D8;
 void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
+  String s = (val) ? "<h1>LED On</h1>" : "<h1>LED Off</h1>";
+  s += "<h2><a href=\"/led/on\">LED On</a> ";
+  s += "<a href=\"/led/off\">LED Off</a></h2>";
+  server.send(200, "text/html", s);
+}
+
+// Метод включения диода
+void ledOn() {
+  val = HIGH; // Включаем диод
+  digitalWrite(led, HIGH);
+  Serial.println(val);
+  // Перенаправление обратно на стартовую страницу
+  server.sendHeader("Location", String("/"), true);
+  server.send ( 302, "text/plain", "");
+}
+
+// Метод выключения диода
+void ledOff() {
+  val = LOW; // Выключаем диод
+  digitalWrite(led, LOW);
+  Serial.println(val);
+  // Перенаправление обратно на стартовую страницу
+  server.sendHeader("Location", String("/"), true);
+  server.send ( 302, "text/plain", "");
 }
 void setup(void){
   pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+  digitalWrite(led,val);
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   WiFi.config(ip, gateway, subnet);
@@ -30,7 +52,9 @@ void setup(void){
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  server.on("/", handleRoot);
+   server.on("/", handleRoot);
+  server.on("/led/on", ledOn);
+  server.on("/led/off", ledOff);
   server.begin();
   Serial.println("HTTP server started");
 }
