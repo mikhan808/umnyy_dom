772d9bddc787d780340377e25b09bc246745287c
mikhan808 <kalyachin@mail.ru>
2019-08-27 14:25:26 +0300

    Разделены файлы для котла и для света в спальне

---
+++ umnyy_dom/kotel/kotel.ino
@@ -0,0 +1,64 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "KALYASHINY";
+const char* password = "09061995";
+ESP8266WebServer server(80);
+
+IPAddress ip(192,168,0,18);  //статический IP
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255,255,255,0);
+bool podogrev;
+int pinKotel=2;
+
+void handleRoot() {
+  String s = "<h1>Котел";
+    s+= (podogrev) ? "включен" : "выключен";
+    s+= "</h1>";
+    s += "<h2><a href=\"/kotel/on\">Включить</a> ";
+    s += "<a href=\"/kotel/off\">Выключить</a></h2>";
+  server.send(200, "text/html; charset=utf-8", s);
+}
+void kotelOn()
+{
+   podogrev=HIGH;
+   digitalWrite(pinKotel, podogrev);
+   server.sendHeader("Location", String("/"), true);
+  server.send ( 302, "text/plain", "");
+}
+void kotelOff()
+{
+   podogrev=LOW;
+   digitalWrite(pinKotel, podogrev);
+   server.sendHeader("Location", String("/"), true);
+  server.send ( 302, "text/plain", "");
+}
+ 
+void setup(){
+  delay(1000);
+  pinMode(pinKotel, OUTPUT);
+  digitalWrite(pinKotel,podogrev);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  WiFi.config(ip, gateway, subnet);
+  Serial.println("");
+  // ожидание соединения
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+   server.on("/", handleRoot);
+  server.on("/kotel/on", kotelOn);
+  server.on("/kotel/off", kotelOff);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+void loop(){
+  server.handleClient();
+}
